03656a3468c85ec448425f7ea5b339feda7a83b0
Miguel Castillo Medina <miguelcm_12@hotmail.com>
2020-11-17 14:13:07 -0500

    variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if(process.env.NODE_ENV  === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe2';
 }else{
-    urlDB = 'mongodb+srv://admin:CsxGfd37HUrOXIOj@cluster0.v9ncq.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
